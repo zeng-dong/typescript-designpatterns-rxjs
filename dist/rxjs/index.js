f066ef16805d8e74df20e672825417ebf5fc17f2
Dong Zeng <dong@freshbytesoftware.com>
2021-07-13 08:11:43 -0500

    error handling before 7

--- dist/rxjs/index.js
+++ dist/rxjs/index.js
@@ -2,3 +2,6 @@
 exports.__esModule = true;
 var external_1 = require("./external");
 external_1.name$.subscribe(function (value) { return console.log(value); });
+//storeDataOnServer("new value").subscribe(value => console.log(value));
+// prior to rxjs7 style
+external_1.storeDataOnServerError("new value").subscribe(function (value) { return console.log(value); }, function (error) { return console.log('Error when saving: ', error.message); });
--- src/rxjs/index.ts
+++ src/rxjs/index.ts
@@ -2,6 +2,14 @@ import {
     name$,
     storeDataOnServer,
     storeDataOnServerError
-  } from './external';
+} from './external';
 
-  name$.subscribe(value => console.log(value));
+name$.subscribe(value => console.log(value));
+
+//storeDataOnServer("new value").subscribe(value => console.log(value));
+
+// prior to rxjs7 style
+storeDataOnServerError("new value").subscribe(
+      value => console.log(value),
+      error => console.log('Error when saving: ', error.message)
+    );
