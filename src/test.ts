d512d263bafeb0f89b288242dedcbad7cdc48ad8
Dong Zeng <dong@freshbytesoftware.com>
2021-07-25 16:34:13 -0500

    basic types

--- src/test.ts
+++ src/test.ts
@@ -4,4 +4,18 @@ function foo(bar: string) {
 
 let baz = 'ABC'
 
-console.log(foo(baz))
+console.log(foo(baz));
+
+
+// a tuple of two elements consisting of a number and a string
+// dictionary if string where the key is a number
+// a Set of numbers
+let tpl: [ number, string];
+let dict: { [key: number]: string};
+let set: Set<number>;
+
+tpl = [1, 'abc'];
+dict = {123: 'abc', 456: 'def'};
+set = new Set([1,23,56]);
+
+
