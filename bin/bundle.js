045d76383de8c184a9558577e69fb7196844b452
Dong Zeng <dong@freshbytesoftware.com>
2021-08-21 21:20:20 -0500

    add webpack and gen bundle

---
+++ bin/bundle.js
@@ -0,0 +1,33 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ (() => {
+
+eval("\r\nfunction foo(bar) {\r\n    return 'Hello, ' + bar;\r\n}\r\nlet baz = 'ABC';\r\nconsole.log(foo(baz));\r\nlet tpl;\r\nlet dict;\r\nlet set;\r\ntpl = [1, 'abc'];\r\ndict = { 123: 'abc', 456: 'def' };\r\nset = new Set([1, 23, 56]);\r\nlet d;\r\nd = [1, 'a', true, ['even', 'another', 'array']];\r\nlet d2;\r\nlet a;\r\na = { 123: 'abc', 456: 'bcd' };\r\nlet b = a[123];\r\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXgudHMuanMiLCJtYXBwaW5ncyI6IkFBQWE7QUFDYjtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSxTQUFTO0FBQ1Q7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBLE1BQU07QUFDTiIsInNvdXJjZXMiOlsid2VicGFjazovL3R5cGVzY3JpcHQtZGVzaWducGF0dGVybnMtcnhqcy8uL3NyYy9pbmRleC50cz9lOTRlIl0sInNvdXJjZXNDb250ZW50IjpbIlwidXNlIHN0cmljdFwiO1xyXG5mdW5jdGlvbiBmb28oYmFyKSB7XHJcbiAgICByZXR1cm4gJ0hlbGxvLCAnICsgYmFyO1xyXG59XHJcbmxldCBiYXogPSAnQUJDJztcclxuY29uc29sZS5sb2coZm9vKGJheikpO1xyXG5sZXQgdHBsO1xyXG5sZXQgZGljdDtcclxubGV0IHNldDtcclxudHBsID0gWzEsICdhYmMnXTtcclxuZGljdCA9IHsgMTIzOiAnYWJjJywgNDU2OiAnZGVmJyB9O1xyXG5zZXQgPSBuZXcgU2V0KFsxLCAyMywgNTZdKTtcclxubGV0IGQ7XHJcbmQgPSBbMSwgJ2EnLCB0cnVlLCBbJ2V2ZW4nLCAnYW5vdGhlcicsICdhcnJheSddXTtcclxubGV0IGQyO1xyXG5sZXQgYTtcclxuYSA9IHsgMTIzOiAnYWJjJywgNDU2OiAnYmNkJyB9O1xyXG5sZXQgYiA9IGFbMTIzXTtcclxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/index.ts\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.ts"]();
+/******/ 	
+/******/ })()
+;
