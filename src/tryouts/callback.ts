9e4a9077c6532eb8123df0251da9478a2c0bfb5a
Dong Zeng <zeng-dong@hotmail.com>
2022-08-09 21:33:17 -0500

    callback in ts more

--- src/tryouts/callback.ts
+++ src/tryouts/callback.ts
@@ -5,3 +5,8 @@ function foo(boo: (s: string) => void) {
 foo(function (a: string): void {
 	console.log(a);
 });
+
+const another = function (myString: string): void {
+	console.log("I am the callback gets passed in to foo");
+};
+foo(another);
