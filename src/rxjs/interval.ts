595915fc759e16cd865401e1b799354b1cae230e
Dong Zeng <zeng-dong@hotmail.com>
2023-08-24 21:06:12 -0500

    interval streaming

--- src/rxjs/interval.ts
+++ src/rxjs/interval.ts
@@ -1,7 +1,17 @@
-import { interval, take } from "rxjs";
+import { interval, map, take } from "rxjs";
 
-const numbers = interval(1000);
+// const numbers = interval(1000);
 
-const takeFourNumbers = numbers.pipe(take(7));
+// const takeFourNumbers = numbers.pipe(take(7));
 
-takeFourNumbers.subscribe((x) => console.log("Next: ", x));
+// takeFourNumbers.subscribe((x) => console.log("Next: ", x));
+
+let next = 200;
+interval(1000)
+	.pipe(
+		map(() => {
+			next = next + Math.floor(Math.random() * 5);
+			return next > 250 ? 222 : next;
+		})
+	)
+	.subscribe((v) => console.log(v));
--- src/rxjs/operators/filter.ts
+++ src/rxjs/operators/filter.ts
@@ -40,3 +40,21 @@ const sportsNewsFeed$ = newsFeed$.pipe(
 newsFeed$
 	.pipe(filter((item) => item.category === "Sports"))
 	.subscribe((i) => console.log(i));
+
+// filter
+// emit only values matching a given condition
+import { of } from "rxjs";
+console.log("# filter even numbers");
+of(1, 2, 3, 4, 5, 6, 7, 8)
+	.pipe(filter((x: number) => x % 2 === 0))
+	.subscribe((x: number) => console.log(x));
+
+const num$ = of(1, 2, 3, 4, 5, 6, 7, 8);
+const oddNum$ = num$.pipe(filter((x) => x % 2 === 1));
+oddNum$.subscribe((x) => console.log(x));
+
+// Output:
+// 2
+// 4
+// 6
+// 8
